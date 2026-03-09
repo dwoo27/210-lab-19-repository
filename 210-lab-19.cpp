2b0154e02046f0227a0a0859ab31befa14db5b0e
dwoo27 <dwooed27@gmail.com>
2026-03-08 20:48:53 -0700

    10 min commit

--- 210-lab-19.cpp
+++ 210-lab-19.cpp
@@ -28,7 +28,50 @@ public:
 
 double randrating();
 void finComments(array<string, NUM_COMMENTS>&);
+
 int main()
 {
     std::cout << "Hello World!\n";
+}
+
+string getTitle(){
+    return title;
+}
+
+void addRev(double rating, string comment) {
+    Node* newReview = new Node;
+    newReview->rating = rating;
+    newReview->comment = comment;
+    newReview->next = nullptr;
+
+    if (!head) { //if list empty, make new review head
+        head = newReview;
+    }
+    else { //otherwise, set new review to be before head, make it new head
+        newReview->next = head;
+        head = newReview;
+    }
+}
+void outputRevs() {
+    if (!head) { //check for empty list
+        cout << "No reviews" << endl;
+        return;
+    }
+    Node* traverse = head; //node for traversing list
+    int count = 0;
+    double total = 0;
+
+    cout << "Outputting all reviews: " << endl;
+    while (traverse) { //while traverse isn't beyond tail
+        cout << "\t> Review #" << ++count << ": " << traverse->rating
+            << ": " << traverse->comment << endl;
+        total += traverse->rating;
+        traverse = traverse->next;
+    }
+
+    cout << "\t> Average: " << (total / count) << endl;
+}
+
+double randrating() {
+    return(rand() % 41 + 10) / 10.0; // rand()%(max-min+1) + min
 }
