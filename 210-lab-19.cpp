ac725bba11140d30aa8e4b7acd1d5e990d8fa66f
dwoo27 <dwooed27@gmail.com>
2026-03-08 20:28:06 -0700

    10 min commit

--- 210-lab-19.cpp
+++ 210-lab-19.cpp
@@ -1,20 +1,29 @@
-// 210-lab-19.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <array>
+#include <ctime>
+using namespace std;
 
+struct Node {
+    double rating;
+    string comment;
+    Node* next;
+};
+
+class Movie {
+    string title;
+    Node* head;
+
+public: 
+    Movie(string t) {
+        title = t;
+    }
+
+    string getTitle(); 
+
+};
 int main()
 {
     std::cout << "Hello World!\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
